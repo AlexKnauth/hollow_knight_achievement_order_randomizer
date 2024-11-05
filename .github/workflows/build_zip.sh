3b73355ab1e020601e01e622e79a8ed347fde3fc
Alex Knauth <alexander@knauth.org>
2024-11-05 02:44:13 -0500

    Update ci.yml: Prepare Release (#3)
    * Update ci.yml
    
    * Update ci.yml: name
    
    * Update ci.yml: Prepare Release
    
    * Update ci.yml: action versions

---
+++ .github/workflows/build_zip.sh
@@ -0,0 +1,34 @@
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    if [[ $OS_NAME =~ ^macos\-.*$ ]]; then
+        stage=$(mktemp -d -t tmp)
+    else
+        stage=$(mktemp -d)
+    fi
+
+    raco exe --gui main.rhm
+    if [[ $OS_NAME =~ ^windows\-.*$ ]]; then
+        raco distribute out main.exe
+    elif [[ $OS_NAME =~ ^macos\-.*$ ]]; then
+        raco distribute out main.app
+    else
+        raco distribute out main
+    fi
+    cp -R "out/" "$stage/" 2>/dev/null || :
+
+    cd $stage
+    if [ "$OS_NAME" = "windows-latest" ]; then
+        7z a $src/hollow-knight-achievement-order-randomizer-$OS_NAME.zip *
+    else
+        tar czf $src/hollow-knight-achievement-order-randomizer-$OS_NAME.tar.gz *
+    fi
+    cd $src
+
+    rm -rf $stage
+}
+
+main
