86c968881624e67d6bcfeccfb152ffe33d0f8a7b
AlexKnauth <alexander@knauth.org>
2024-11-11 22:54:06 -0500

    Delete clipboard.rkt

--- util/clipboard.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(provide set_clipboard_string)
-
-(require racket/gui/base
-         racket/class)
-
-(define (set_clipboard_string s)
-  (send the-clipboard set-clipboard-string s 0))
